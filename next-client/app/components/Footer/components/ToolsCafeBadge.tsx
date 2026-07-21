1724928b05a712b9b378735d9ab96c34b10ca119
Mares Popa <office@marespopa.com>
2026-07-21 16:08:13 +0000

    fix: use image

--- next-client/app/components/Footer/components/ToolsCafeBadge.tsx
+++ next-client/app/components/Footer/components/ToolsCafeBadge.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
 import React from "react";
 
 export default function ToolsCafeBadge() {
   return (
     <a href="https://tools.cafe/p/tool-1784368796535" target="_blank" rel="noopener">
-      <Image
-        src="https://tools.cafe/badge.png"
-        alt="Listed on tools.cafe"
-        width={180}
-        height={54}
-        unoptimized
-      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="https://tools.cafe/badge.png" alt="Listed on tools.cafe" width="180" height="54" />
     </a>
   );
 }
